9c7843e2799d27b115651a7c38d8d9c37e5f5bd9
Scott Remsen <X2SBREMS@southernco.com>
2021-09-03 15:34:45 -0500

    Added whitespace

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,5 +8,6 @@ export const environment = {
     storageBucket: "sbrmusicprofile.appspot.com",
     messagingSenderId: "1066240430044",
     appId: "1:1066240430044:web:2f7759a03faac5f09cc919"
+
   }
 };
